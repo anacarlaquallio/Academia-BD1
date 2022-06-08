8144af77429965286abbab6d3dfb891b7076790a
Ana Carla Quallio Rosa <anacarlarosa@alunos.utfpr.edu.br>
2022-06-08 15:18:11 -0300

    add insercao funcionario

---
+++ Entrega 3 - Inserts/insercao.sql
@@ -0,0 +1,105 @@
+INSERT INTO FUNCIONARIO (cpf, nome, dataContratacao) VALUES 
+("05650090602", "Valquiria Fernandes dos Santos Palmeira", "20/06/2008"), ("06601775606", "Marta Siqueira Neves", "13/09/2012"),
+("15249340628", "Paulo Richard Vieira Fonseca Rosa", "03/02/2015"),
+("11365492770", "Thiago Oliveira Silva Martins", "20/06/2008"),
+("08594743645", "Luciana Cardoso Gonçalves", "20/02/2012"),
+("44702515691", "Willer Bartho Vieira", "10/05/2020"),
+("53467965687", "Sérgio Leonardo Teixeira", "21/06/2008"),
+("00199828601", "Claudia Moreira Santos", "22/06/2008"),
+("02572188589", "Kátia Borges Alecrim", "22/06/2008"),
+("45062727812", "Amanda Beatriz Ponpeo", "07/10/2010"),
+
+("60586941320", "Abner Silva Sarrazin", "02/11/2008"),
+("07671368458", "Cleiton Alves Feitosa", "12/06/2008"),
+("05842364417", "Karine Maria Gonçalves Cortez", "25/04/2021"),
+("07673414490", "Gustavo Pereira dos Santos", "30/07/2008"),
+("09099983450", "Leandro José Paulo da Silva", "23/02/2016"),
+("09590535496", "Maria Nainam Silvino Araújo dos Santos", "17/08/2011"),
+("06075815490", "Jullieny Melo de Oliveira", "12/12/2012"),
+("07526066462", "Carlos André de Holanda Florêncio e Silva", "28/06/2008"),
+("03406454402", "Carolina Caraciolo de Souza", "21/04/2022"),
+("02314423437", "Alcione Monteiro Lucas Germino", "19/11/2011");
+
+INSERT INTO FUNCIONARIO_PERSONAL (cpfPersonal, telefone, diploma, planoContratacao) VALUES 
+("05650090602", "44984151939", "Bacharelado em Educação Física", 
+"Pelo presente instrumento particular de PRESTAÇÃO DE SERVIÇOS, Valquiria Fernandes dos Santos Palmeira, brasileira, residente e domiciliada em Campo Mourão, portadora do CPF 056.500.906-02, aqui denominada CONTRATADO, pactua o presente contrato de Prestação de Serviços em conformidade das cláusulas seguintes:
+PRIMEIRA - Sendo o CONTRATADO, Bacharel ou Licenciado em Educação Física atuando na Área de PERSONAL TRAINING, o contrato tem por objetivo a prestação de serviços de treinamento para a academia, dentro das individualidades de cada cliente, resultado da avaliação física elaborada pelo CONTRATADO.
+SEGUNDA - O prazo do presente CONTRATO é de no mínimo 03 (três) meses, iniciando-se a partir da assinatura do mesmo. Podendo ser renovado por igual período ou superior na quinzena que antecede o seu término.
+TERCEIRA - Os programas de treinamento serão desenvolvidos nas dependências desta academia. As aulas serão ministradas com tempo mínimo de 60 (sessenta minutos) de forma individual para cada cliente.
+QUARTA - O salário base do CONTRATADO será o valordois salários mínimos mínimo adotado no valor adotado pelo piso nacional, com reajustes da inflação anual, mais o percentual da quantidade de clientes atendidos mensalmente pelo CONTRATADO.
+E por assim ter contratado, lavra e assina o presente instrumento em duas vias de igual teor e forma."), 
+("06601775606", "41999871325", "Bacharelado em Educação Física",
+"Pelo presente instrumento particular de PRESTAÇÃO DE SERVIÇOS, Marta Siqueira Neves, brasileira, residente e domiciliada em Campo Mourão, portadora do CPF 066.017.756-06, aqui denominada CONTRATADO, pactua o presente contrato de Prestação de Serviços em conformidade das cláusulas seguintes:
+PRIMEIRA - Sendo o CONTRATADO, Bacharel ou Licenciado em Educação Física atuando na Área de PERSONAL TRAINING, o contrato tem por objetivo a prestação de serviços de treinamento para a academia, dentro das individualidades de cada cliente, resultado da avaliação física elaborada pelo CONTRATADO.
+SEGUNDA - O prazo do presente CONTRATO é de no mínimo 03 (três) meses, iniciando-se a partir da assinatura do mesmo. Podendo ser renovado por igual período ou superior na quinzena que antecede o seu término.
+TERCEIRA - Os programas de treinamento serão desenvolvidos nas dependências desta academia. As aulas serão ministradas com tempo mínimo de 60 (sessenta minutos) de forma individual para cada cliente.
+QUARTA - O salário base do CONTRATADO será o valor mínimo adotado pelo piso nacional, com reajustes da inflação anual, mais o percentual da quantidade de clientes atendidos mensalmente pelo CONTRATADO.
+E por assim ter contratado, lavra e assina o presente instrumento em duas vias de igual teor e forma."),
+("15249340628", "44984362872", "Bacharelado em Educação Física",
+"Pelo presente instrumento particular de PRESTAÇÃO DE SERVIÇOS, Paulo Richard Vieira Fonseca Rosa, brasileiro, residente e domiciliado em Campo Mourão, portador do CPF 152.493.406-28, aqui denominado CONTRATADO, pactua o presente contrato de Prestação de Serviços em conformidade das cláusulas seguintes:
+PRIMEIRA - Sendo o CONTRATADO, Bacharel ou Licenciado em Educação Física atuando na Área de PERSONAL TRAINING, o contrato tem por objetivo a prestação de serviços de treinamento para a academia, dentro das individualidades de cada cliente, resultado da avaliação física elaborada pelo CONTRATADO.
+SEGUNDA - O prazo do presente CONTRATO é de no mínimo 03 (três) meses, iniciando-se a partir da assinatura do mesmo. Podendo ser renovado por igual período ou superior na quinzena que antecede o seu término.
+TERCEIRA - Os programas de treinamento serão desenvolvidos nas dependências desta academia. As aulas serão ministradas com tempo mínimo de 60 (sessenta minutos) de forma individual para cada cliente.
+QUARTA - O salário base do CONTRATADO será o valor mínimo adotado pelo piso nacional, com reajustes da inflação anual, mais o percentual da quantidade de clientes atendidos mensalmente pelo CONTRATADO.
+E por assim ter contratado, lavra e assina o presente instrumento em duas vias de igual teor e forma."),
+("11365492770", "44998781248", "Bacharelado em Educação Física",
+"Pelo presente instrumento particular de PRESTAÇÃO DE SERVIÇOS, Thiago Oliveira Silva Martins, brasileiro, residente e domiciliado em Campo Mourão, portador do CPF 113.654.927-70, aqui denominado CONTRATADO, pactua o presente contrato de Prestação de Serviços em conformidade das cláusulas seguintes:
+PRIMEIRA - Sendo o CONTRATADO, Bacharel ou Licenciado em Educação Física atuando na Área de PERSONAL TRAINING, o contrato tem por objetivo a prestação de serviços de treinamento para a academia, dentro das individualidades de cada cliente, resultado da avaliação física elaborada pelo CONTRATADO.
+SEGUNDA - O prazo do presente CONTRATO é de no mínimo 03 (três) meses, iniciando-se a partir da assinatura do mesmo. Podendo ser renovado por igual período ou superior na quinzena que antecede o seu término.
+TERCEIRA - Os programas de treinamento serão desenvolvidos nas dependências desta academia. As aulas serão ministradas com tempo mínimo de 60 (sessenta minutos) de forma individual para cada cliente.
+QUARTA - O salário base do CONTRATADO será o valor mínimo adotado pelo piso nacional, com reajustes da inflação anual, mais o percentual da quantidade de clientes atendidos mensalmente pelo CONTRATADO.
+E por assim ter contratado, lavra e assina o presente instrumento em duas vias de igual teor e forma."),
+("08594743645", "44997843265", "Licenciatura em Educação Física",
+"Pelo presente instrumento particular de PRESTAÇÃO DE SERVIÇOS, Luciana Cardoso Gonçalves, brasileira, residente e domiciliada em Campo Mourão, portadora do CPF 085.947.436-45, aqui denominada CONTRATADO, pactua o presente contrato de Prestação de Serviços em conformidade das cláusulas seguintes:
+PRIMEIRA - Sendo o CONTRATADO, Bacharel ou Licenciado em Educação Física atuando na Área de PERSONAL TRAINING, o contrato tem por objetivo a prestação de serviços de treinamento para a academia, dentro das individualidades de cada cliente, resultado da avaliação física elaborada pelo CONTRATADO.
+SEGUNDA - O prazo do presente CONTRATO é de no mínimo 03 (três) meses, iniciando-se a partir da assinatura do mesmo. Podendo ser renovado por igual período ou superior na quinzena que antecede o seu término.
+TERCEIRA - Os programas de treinamento serão desenvolvidos nas dependências desta academia. As aulas serão ministradas com tempo mínimo de 60 (sessenta minutos) de forma individual para cada cliente.
+QUARTA - O salário base do CONTRATADO será o valor mínimo adotado pelo piso nacional, com reajustes da inflação anual, mais o percentual da quantidade de clientes atendidos mensalmente pelo CONTRATADO.
+E por assim ter contratado, lavra e assina o presente instrumento em duas vias de igual teor e forma."),
+("44702515691", "44987653936", "Bacharelado em Educação Física",
+"Pelo presente instrumento particular de PRESTAÇÃO DE SERVIÇOS, Willer Bartho Vieira, brasileiro, residente e domiciliado em Campo Mourão, portador do CPF 447.025.156-91, aqui denominado CONTRATADO, pactua o presente contrato de Prestação de Serviços em conformidade das cláusulas seguintes:
+PRIMEIRA - Sendo o CONTRATADO, Bacharel ou Licenciado em Educação Física atuando na Área de PERSONAL TRAINING, o contrato tem por objetivo a prestação de serviços de treinamento para a academia, dentro das individualidades de cada cliente, resultado da avaliação física elaborada pelo CONTRATADO.
+SEGUNDA - O prazo do presente CONTRATO é de no mínimo 03 (três) meses, iniciando-se a partir da assinatura do mesmo. Podendo ser renovado por igual período ou superior na quinzena que antecede o seu término.
+TERCEIRA - Os programas de treinamento serão desenvolvidos nas dependências desta academia. As aulas serão ministradas com tempo mínimo de 60 (sessenta minutos) de forma individual para cada cliente.
+QUARTA - O salário base do CONTRATADO será o valor mínimo adotado pelo piso nacional, com reajustes da inflação anual, mais o percentual da quantidade de clientes atendidos mensalmente pelo CONTRATADO.
+E por assim ter contratado, lavra e assina o presente instrumento em duas vias de igual teor e forma."),
+("53467965687", "43991780602", "Bacharelado em Educação Física",
+"Pelo presente instrumento particular de PRESTAÇÃO DE SERVIÇOS, Sérgio Leonardo Teixeira, brasileiro, residente e domiciliado em Campo Mourão, portador do CPF 534.679.656-87, aqui denominado CONTRATADO, pactua o presente contrato de Prestação de Serviços em conformidade das cláusulas seguintes:
+PRIMEIRA - Sendo o CONTRATADO, Bacharel ou Licenciado em Educação Física atuando na Área de PERSONAL TRAINING, o contrato tem por objetivo a prestação de serviços de treinamento para a academia, dentro das individualidades de cada cliente, resultado da avaliação física elaborada pelo CONTRATADO.
+SEGUNDA - O prazo do presente CONTRATO é de no mínimo 03 (três) meses, iniciando-se a partir da assinatura do mesmo. Podendo ser renovado por igual período ou superior na quinzena que antecede o seu término.
+TERCEIRA - Os programas de treinamento serão desenvolvidos nas dependências desta academia. As aulas serão ministradas com tempo mínimo de 60 (sessenta minutos) de forma individual para cada cliente.
+QUARTA - O salário base do CONTRATADO será o valor mínimo adotado pelo piso nacional, com reajustes da inflação anual, mais o percentual da quantidade de clientes atendidos mensalmente pelo CONTRATADO.
+E por assim ter contratado, lavra e assina o presente instrumento em duas vias de igual teor e forma."),
+("00199828601", "44998564321", "Bacharelado em Educação Física",
+"Pelo presente instrumento particular de PRESTAÇÃO DE SERVIÇOS, Claudia Moreira Santos, brasileira, residente e domiciliada em Campo Mourão, portadora do CPF 001.998.286-01, aqui denominada CONTRATADO, pactua o presente contrato de Prestação de Serviços em conformidade das cláusulas seguintes:
+PRIMEIRA - Sendo o CONTRATADO, Bacharel ou Licenciado em Educação Física atuando na Área de PERSONAL TRAINING, o contrato tem por objetivo a prestação de serviços de treinamento para a academia, dentro das individualidades de cada cliente, resultado da avaliação física elaborada pelo CONTRATADO.
+SEGUNDA - O prazo do presente CONTRATO é de no mínimo 03 (três) meses, iniciando-se a partir da assinatura do mesmo. Podendo ser renovado por igual período ou superior na quinzena que antecede o seu término.
+TERCEIRA - Os programas de treinamento serão desenvolvidos nas dependências desta academia. As aulas serão ministradas com tempo mínimo de 60 (sessenta minutos) de forma individual para cada cliente.
+QUARTA - O salário base do CONTRATADO será o valor mínimo adotado pelo piso nacional, com reajustes da inflação anual, mais o percentual da quantidade de clientes atendidos mensalmente pelo CONTRATADO.
+E por assim ter contratado, lavra e assina o presente instrumento em duas vias de igual teor e forma."),
+("02572188589", "41984325698", "Bacharelado em Educação Física",
+"Pelo presente instrumento particular de PRESTAÇÃO DE SERVIÇOS, Kátia Borges Alecrim, brasileira, residente e domiciliada em Campo Mourão, portadora do CPF 025.721.885-89, aqui denominada CONTRATADO, pactua o presente contrato de Prestação de Serviços em conformidade das cláusulas seguintes:
+PRIMEIRA - Sendo o CONTRATADO, Bacharel ou Licenciado em Educação Física atuando na Área de PERSONAL TRAINING, o contrato tem por objetivo a prestação de serviços de treinamento para a academia, dentro das individualidades de cada cliente, resultado da avaliação física elaborada pelo CONTRATADO.
+SEGUNDA - O prazo do presente CONTRATO é de no mínimo 03 (três) meses, iniciando-se a partir da assinatura do mesmo. Podendo ser renovado por igual período ou superior na quinzena que antecede o seu término.
+TERCEIRA - Os programas de treinamento serão desenvolvidos nas dependências desta academia. As aulas serão ministradas com tempo mínimo de 60 (sessenta minutos) de forma individual para cada cliente.
+QUARTA - O salário base do CONTRATADO será o valor mínimo adotado pelo piso nacional, com reajustes da inflação anual, mais o percentual da quantidade de clientes atendidos mensalmente pelo CONTRATADO.
+E por assim ter contratado, lavra e assina o presente instrumento em duas vias de igual teor e forma."),
+("45062727812", "44995378415", "Bacharelado em Educação Física",
+"Pelo presente instrumento particular de PRESTAÇÃO DE SERVIÇOS, Amanda Beatriz Ponpeo, brasileira, residente e domiciliada em Campo Mourão, portadora do CPF 450.627.278-12, aqui denominada CONTRATADO, pactua o presente contrato de Prestação de Serviços em conformidade das cláusulas seguintes:
+PRIMEIRA - Sendo o CONTRATADO, Bacharel ou Licenciado em Educação Física atuando na Área de PERSONAL TRAINING, o contrato tem por objetivo a prestação de serviços de treinamento para a academia, dentro das individualidades de cada cliente, resultado da avaliação física elaborada pelo CONTRATADO.
+SEGUNDA - O prazo do presente CONTRATO é de no mínimo 03 (três) meses, iniciando-se a partir da assinatura do mesmo. Podendo ser renovado por igual período ou superior na quinzena que antecede o seu término.
+TERCEIRA - Os programas de treinamento serão desenvolvidos nas dependências desta academia. As aulas serão ministradas com tempo mínimo de 60 (sessenta minutos) de forma individual para cada cliente.
+QUARTA - O salário base do CONTRATADO será o valor mínimo adotado pelo piso nacional, com reajustes da inflação anual, mais o percentual da quantidade de clientes atendidos mensalmente pelo CONTRATADO.
+E por assim ter contratado, lavra e assina o presente instrumento em duas vias de igual teor e forma.");
+
+INSERT INTO FUNCIONARIO_SECRETARIA (cpfSecretaria, email, salario, capacitacao) VALUES 
+("60586941320", "abner.sarrazin@gmail.com", 2100.00, "Graduação em Administração"),
+("07671368458", "cleitonafeitosa@yahoo.com.br", 2100.00, "Graduação em Administração"),
+("05842364417", "karinecortez@hotmail.com", 2100.00, "Graduação em Administração"),
+("07673414490", "gustavos.santos@gmail.com", 2100.00, "Graduação em Administração"),
+("09099983450", "leandrojpsilva@gmail.com", 2100.00, "Graduação em Administração"),
+("09590535496", "marian.santos@outlook.com", 2500.00, "Especialização em Administração"),
+("06075815490", "jullienyoliveira@hotmail.com", 2100.00, "Graduação em Administração"),
+("07526066462", "carlosahlandasilva@hotmail.com", 2100.00, "Graduação em Administração"),
+("03406454402", "cacazouza@gmail.com", 2100.00, "Graduação em Administração"),
+("02314423437", "alcione.germino@hotmail.com", 2100.00, "Graduação em Administração");
